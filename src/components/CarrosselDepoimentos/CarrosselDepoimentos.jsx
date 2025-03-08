794c96d817c4b0f029b36fe303828a2428721836
PauloAdson <paulo.alves200581@gmail.com>
2025-03-08 12:09:44 -0300

    style: adicionando um novo cliente e trocando algumas cores

--- src/components/CarrosselDepoimentos/CarrosselDepoimentos.jsx
+++ src/components/CarrosselDepoimentos/CarrosselDepoimentos.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 import "./styles.css";
-import Depoimento from '../Depoimento/Depoimento';
+import Depoimento from "../Depoimento/Depoimento";
+import logoAzevedoAdvocacia from "../../assets/images/logos-clientes/azevedo-advocacia-logo.png";
+import logoFurlanSolucoes from "../../assets/images/logos-clientes/logo-furlan-solucoes.png";
 
 const CarrosselDepoimentos = () => {
-    return (
-        <div className='depoimentos__carrosel'>
-            <Depoimento bgLogo={"white"} cliente={"Azevedo Advocacia"} comentario={"E ficou muito top ! Trabalho incrível 👏 agradecemos muito pelo empenho em compreender e nos explicar cada passo."} />
-        </div>
-    )
-}
+  return (
+    <div className="depoimentos__carrosel">
+      <Depoimento
+        bgLogo={"white"}
+        logoCliente={logoAzevedoAdvocacia}
+        cliente={"Azevedo Advocacia"}
+        comentario={
+          "E ficou muito top ! Trabalho incrível 👏 agradecemos muito pelo empenho em compreender e nos explicar cada passo."
+        }
+      />
+      <Depoimento
+        bgLogo={"black"}
+        cliente={"Furlan Soluções Financeiras"}
+        logoCliente={logoFurlanSolucoes}
+        comentario={"Gostei muito👏"}
+      />
+    </div>
+  );
+};
 
-export default CarrosselDepoimentos
+export default CarrosselDepoimentos;
--- src/components/Depoimento/Depoimento.jsx
+++ src/components/Depoimento/Depoimento.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 import "./styles.css";
-import logoAzevedoAdvocacia from "../../assets/images/logos-clientes/azevedo-advocacia-logo.png"
-import { Link } from 'react-router-dom';
+import logoAzevedoAdvocacia from "../../assets/images/logos-clientes/azevedo-advocacia-logo.png";
+import { Link } from "react-router-dom";
 
-const Depoimento = ({ bgLogo, cliente, comentario }) => {
-    return (
-        <>
-            <div className='depoimento'>
-                <Link
-                    className={`depoimento__container-logo ${bgLogo}`}
-                    to={"https://asazevedo.adv.br/"}
-                    target='_blank'
-                    rel="noopener">
-                    <img className='depoimento__logo' src={logoAzevedoAdvocacia} alt={`Cliente: ${cliente}`} title={cliente} />
-                </Link>
-                <span className='depoimento__cliente'>{cliente}</span>
-                <p className='depoimento__comentario'>{comentario}</p>
-            </div>
+const Depoimento = ({ bgLogo, cliente, logoCliente, comentario }) => {
+  return (
+    <>
+      <div className="depoimento">
+        <Link
+          className={`depoimento__container-logo ${bgLogo}`}
+          to={"https://asazevedo.adv.br/"}
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            className="depoimento__logo"
+            src={logoCliente}
+            alt={`Cliente: ${cliente}`}
+            title={cliente}
+          />
+        </Link>
+        <span className="depoimento__cliente">{cliente}</span>
+        <p className="depoimento__comentario">{comentario}</p>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Depoimento
+export default Depoimento;
