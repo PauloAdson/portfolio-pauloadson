a0ab9fefb21b8acb78d0cf9dcdcbd467f5b7be0a
PauloAdson <paulo.alves200581@gmail.com>
2025-03-08 13:09:50 -0300

    alterando o link do redirecionamento

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -42,7 +42,7 @@ export class Contato extends React.Component {
               <input
                 type="hidden"
                 name="redirectTo"
-                value={"https://www.pauloadson.com.br/obrigado"}
+                value={"pauloadson.com.br/obrigado"}
               />
             </form>
           </section>
