460744df2692bc1126b4a2878ccbe3dc63ee5654
PauloAdson <paulo.alves200581@gmail.com>
2025-03-08 13:07:21 -0300

    teste de redirecionamento

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import { Contato } from "./pages/Contato/Contato";
-import { Obrigado } from "./pages/Obrigado/Obrigado";
+import Obrigado from "./pages/Obrigado/Obrigado";
 import ScrollToHash from "./components/ScrollToHash/ScrollToHash";
 
 const App = () => {
